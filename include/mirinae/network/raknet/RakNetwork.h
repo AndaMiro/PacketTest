110a66a5d86e636c68581669447a881a4dbe8aeb
AndaMiro <superwhyee@gmail.com>
2025-09-07 06:17:10 +0900

    bye~

--- include/mirinae/network/raknet/RakNetwork.h
+++ include/mirinae/network/raknet/RakNetwork.h
@@ -2,6 +2,30 @@
 #include <mirinae/network/UdpNetwork.h>
 
 namespace mirinae::network::raknet{
+    enum class Reliability : uint8_t{
+        UNRELIABLE, 
+        RELIABLE
+    };
+
+    struct Datagram{
+        uint8_t* data;
+        uint16_t len;
+        uint16_t mtu;
+    };
+
+    struct Payload{
+        uint16_t len;
+        uint16_t poolId;
+        std::atomic<uint16_t> refCnt{1};
+        uint8_t* data;
+    };
+
+    struct InternalPacket{
+        uint8_t pid;
+        Reliability flags;
+        Payload* payload;
+    };
+
 	using PacketCallback = std::function<void(const Endpoint& endpoint, const int id, const void* data, std::size_t n)>;
 
     class RakNetwork{
